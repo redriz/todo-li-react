fd80f0aa8a3ee923f0bf33be985e876823c31dc0
redriz <redriz@teste.com>
2025-03-21 21:44:23 +0000

    feat: implement task deletion functionality in Tasks component

--- src/App.jsx
+++ src/App.jsx
@@ -35,6 +35,11 @@ function App() {
     setTasks(newTasks);
   }
 
+  function onDeleteTaskClick(taskId) {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTasks);
+  }
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px]">
@@ -42,7 +47,11 @@ function App() {
           Lista de Tarefas
         </h1>
         <AddTask />
-        <Tasks tasks={tasks} onTaskClick={onTaskClick} />
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onDeleteTaskClick={onDeleteTaskClick}
+        />
       </div>
     </div>
   );
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,19 +1,26 @@
-import { ChevronRightIcon } from "lucide-react";
+import { ChevronRightIcon, Trash } from "lucide-react";
 
-function Tasks(props) {
+function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
-      {props.tasks.map((task) => (
+      {tasks.map((task) => (
         <li key={task.id} className="flex gap-2">
           <button
-            onClick={() => props.onTaskClick(task.id)}
-            className="bg-slate-400 text-left w-full text-white p-2 rounded-md"
+            onClick={() => onTaskClick(task.id)}
+            className={`bg-slate-400 text-left font-medium w-full text-white p-2 rounded-md ${
+              task.isCompleted && "bg-slate-300 text-slate-500 line-through"
+            }`}
           >
             {task.title}
-            {task.isCompleted ? "COMPLETE" : "INCOMPLETE"}
           </button>
           <button className="bg-slate-400 text-white p-2 rounded-md">
             <ChevronRightIcon />
+          </button>
+          <button
+            onClick={() => onDeleteTaskClick(task.id)}
+            className="bg-slate-400 text-white p-2 rounded-md"
+          >
+            <Trash />
           </button>
         </li>
       ))}

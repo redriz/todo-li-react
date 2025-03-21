8675f81687358855caf98b369f9724c0aabe55aa
redriz <redriz@teste.com>
2025-03-21 21:18:33 +0000

    feat: add task completion toggle functionality in Tasks component

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,17 @@ function App() {
     },
   ]);
 
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+
+      return task;
+    });
+    setTasks(newTasks);
+  }
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px]">
@@ -31,7 +42,7 @@ function App() {
           Lista de Tarefas
         </h1>
         <AddTask />
-        <Tasks tasks={tasks} />
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} />
       </div>
     </div>
   );
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -5,8 +5,12 @@ function Tasks(props) {
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
       {props.tasks.map((task) => (
         <li key={task.id} className="flex gap-2">
-          <button className="bg-slate-400 text-left w-full text-white p-2 rounded-md">
+          <button
+            onClick={() => props.onTaskClick(task.id)}
+            className="bg-slate-400 text-left w-full text-white p-2 rounded-md"
+          >
             {task.title}
+            {task.isCompleted ? "COMPLETE" : "INCOMPLETE"}
           </button>
           <button className="bg-slate-400 text-white p-2 rounded-md">
             <ChevronRightIcon />

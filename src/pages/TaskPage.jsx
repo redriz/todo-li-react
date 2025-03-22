af94ecbe41ba852234520429d8820c418673449a
redriz <redriz@teste.com>
2025-03-22 19:05:22 +0000

    feat: enhance TaskPage layout and add navigation button for task details

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -1,13 +1,30 @@
-import { useSearchParams } from "react-router-dom";
+import { ChevronLeftIcon } from "lucide-react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 function TaskPage() {
+  const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const title = searchParams.get("title");
   const description = searchParams.get("description");
   return (
-    <div className="w-screen h-screen bg-slate-500 p-6">
-      <h1>{title}</h1>
-      <p>{description}</p>
+    <div className="w-screen h-screen bg-slate-500 p-6 justify-center flex">
+      <div className="w-[500px] space-y-4">
+        <div className="flex justify-center relative mb-6">
+          <button
+            className="absolute left-0 top-0 bottom-0 bg-slate-600 p-1 rounded-md text-slate-100"
+            onClick={() => navigate(-1)}
+          >
+            <ChevronLeftIcon />
+          </button>
+          <h1 className="text-3xl text-slate-100 font-bold text-center">
+            Detalhes da Tarefa
+          </h1>
+        </div>
+        <div className="p-6 bg-slate-200 rounded-md shadow">
+          <h2 className="text-slate-700 font-bold text-xl">{title}</h2>
+          <p className="text-slate-700">{description}</p>
+        </div>
+      </div>
     </div>
   );
 }

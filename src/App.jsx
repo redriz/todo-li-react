bd39073b0ef3174c2880d8715b6a5cdd87d4201b
redriz <redriz@teste.com>
2025-03-22 19:39:37 +0000

    feat: persist tasks in local storage and load on app initialization

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
 import { v4 as uuidv4 } from "uuid";
 
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      title: "Estudar programação",
-      description: "Estudar programação com foco em ReactJS",
-      isCompleted: false,
-    },
-    {
-      id: 2,
-      title: "Estudar inglês",
-      description: "Estudar inglês com foco em leitura e escrita",
-      isCompleted: false,
-    },
-    {
-      id: 3,
-      title: "Fazer exercícios",
-      description: "Fazer exercícios físicos",
-      isCompleted: false,
-    },
-  ]);
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   function onTaskClick(taskId) {
     const newTasks = tasks.map((task) => {
